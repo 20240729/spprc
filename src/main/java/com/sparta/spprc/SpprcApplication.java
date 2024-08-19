473d5548c2cca0871c4c2495eb030f6fa905bacb
20240729 <yoohyeonsik@gmail.com>
2024-08-19 19:43:12 +0900

    a

---
+++ src/main/java/com/sparta/spprc/SpprcApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spprc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpprcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpprcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/spprc/SpprcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spprc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpprcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
